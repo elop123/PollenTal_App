ce986813f140966e67dee416445d0bdba281deca
elop123 <142408289+elop123@users.noreply.github.com>
2024-04-22 15:32:10 +0200

    sw

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ self.addEventListener('install', event => {
 	console.log('Service Worker has been installed');
 })
 
-// Install Service Worker
+// Activate Service Worker
 self.addEventListener('activate', event => {
 	console.log('Service Worker has been activated');
 })
